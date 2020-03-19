b7d3648bd3d195dbae1e39aa74ac875670125cd0
cgpalmer <cgpalmer91@gmail.com>
2020-03-19 22:10:06 +0000

    updated test page

--- main.js
+++ main.js
@@ -1,3 +1,46 @@
-$(".number").click(function(){
-    alert("JS is working!");
+const calculator = {
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
+
+function updateDisplay() {
+  const display = document.querySelector('.calculator-screen');
+  display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+  const { target } = event;
+  if (!target.matches('button')) {
+    return;
+  }
+
+ inputDigit(target.value);
+updateDisplay();
 });
+
+function inputDigit(digit) {
+  const { displayValue } = calculator;
+  // Overwrite `displayValue` if the current value is '0' otherwise append to it
+  calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+}
+
+function handleOperator(nextOperator) {
+  const { firstOperand, displayValue, operator } = calculator
+  const inputValue = parseFloat(displayValue);
+
+  if (firstOperand === null) {
+    calculator.firstOperand = inputValue;
+  }
+
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+}
+
+handleOperator(target.value);
+updateDisplay();
---
+++ readme.md
@@ -0,0 +1,2 @@
+Thank you to this website for giving me the code on how to get the numbers to pop up on the screen.
+https://freshman.tech/calculator/
